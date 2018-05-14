ba25c7b80e922b6a45743521798e731885e8abcc
Iwark <iwark02@gmail.com>
2018-05-14 23:58:50 +0900

    Show all groups

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @groups = current_user.groups.order(:display_order).includes([{accounts: :target}, :message_pattern])
+    # @groups = current_user.groups.order(:display_order).includes([{accounts: :target}, :message_pattern])
+    @groups = Group.all.order(:display_order).includes([{accounts: :target}, :message_pattern])
   end
 end
